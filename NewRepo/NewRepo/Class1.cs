35827ff6529c2d84086dd65b079ef6961f343194
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-28 20:57:58 +0530

    second commit

---
+++ NewRepo/NewRepo/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewRepo
+{
+    class Class1
+    {
+        public void Noor()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
