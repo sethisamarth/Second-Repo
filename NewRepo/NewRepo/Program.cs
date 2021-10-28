3f3e4d603449c41c89c05c7049fc0bfbd4eade0d
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-28 20:47:49 +0530

    first commit

---
+++ NewRepo/NewRepo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NewRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to new repo");
+        }
+        public void Demo()
+        {
+            Console.WriteLine("Your placed");
+        }
+   
+    }
+}
